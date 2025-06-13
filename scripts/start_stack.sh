aa36b069e3ac23f183e6df123f58aaf3a890e71d
markmur90 <markmur90@proton.me>
2025-06-14 00:20:44 +0200

    modificados:     config/torrc_simulador 	modificados:     logs/gunicorn_error.log 	modificados:     logs/supervisord.log 	modificados:     logs/supervisord.pid 	modificados:     scripts/start_stack.sh 	renombrados:     scripts/start_stack copy.sh -> scripts/start_stack2.sh

--- scripts/start_stack.sh
+++ scripts/start_stack.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # ─── Configuración ─────────────────────────────────────────────
-BASE_DIR="$HOME/Simulador"
+BASE_DIR="/home/markmur88/Simulador"
 SIM_DIR="$BASE_DIR/simulador_banco"
 TOR_DATA="$BASE_DIR/tor_data/hidden_service"
 SUPERVISORD_CONF="$BASE_DIR/config/supervisor_simulador.conf"
@@ -13,9 +13,12 @@ LOG_DIR="$BASE_DIR/logs"
 REQS_PATH="$HOME/api_bank_h2/requirements.txt"
 VENV_ACT="$HOME/envAPP/bin/activate"
 
+
+
+
 # ─── 0) Pre-check de torrc ──────────────────────────────────────
 echo "🔍 Verificando torrc…"
-tor --verify-config -f "$TORRC" \
+sudo tor --verify-config -f "$TORRC" \
   || { echo "❌ Error en torrc, corrígelo antes de continuar"; exit 1; }
 
 echo ""
@@ -65,10 +68,14 @@ echo ""
 
 # ─── 4) Ajustar permisos Tor data & logs ──────────────────────
 echo "🔐 Permisos de tor_data y logs…"
-mkdir -p "$LOG_DIR" "$TOR_DATA"
-chown -R "$(whoami)" "$BASE_DIR/tor_data" "$LOG_DIR"
-chmod -R 700 "$BASE_DIR/tor_data"
-chmod -R 755 "$LOG_DIR"
+# mkdir -p "$LOG_DIR" "$TOR_DATA"
+# chown -R "$(whoami)" "$BASE_DIR/tor_data" "$LOG_DIR"
+# chmod -R 700 "$BASE_DIR/tor_data"
+# chmod -R 755 "$LOG_DIR"
+
+sudo chown -R markmur88 /home/markmur88/Simulador/tor_data/hidden_service
+sudo chmod 700                 /home/markmur88/Simulador/tor_data/hidden_service
+sudo systemctl restart tor
 
 echo ""
 sleep 3
@@ -76,15 +83,15 @@ echo ""
 
 # ─── 5) Supervisord: reread/update & arrancar servicios ───────
 echo "♻️  Recargando supervisord…"
-supervisorctl -c "$SUPERVISORD_CONF" reread
-supervisorctl -c "$SUPERVISORD_CONF" update
+sudo supervisorctl -c "$SUPERVISORD_CONF" reread
+sudo supervisorctl -c "$SUPERVISORD_CONF" update
 
 echo ""
 sleep 3
 echo ""
 
 echo "▶️ Iniciando servicios con supervisord…"
-supervisorctl -c "$SUPERVISORD_CONF" start all \
+sudo supervisorctl -c "$SUPERVISORD_CONF" start all \
   || { echo "❌ No se pudieron iniciar todos los servicios"; exit 1; }
 
 echo ""
@@ -116,7 +123,7 @@ echo ""
 
 # ─── 7) Estado final ──────────────────────────────────────────
 echo ""
-supervisorctl -c "$SUPERVISORD_CONF" status
+sudo supervisorctl -c "$SUPERVISORD_CONF" status
 
 echo ""
 sleep 3
