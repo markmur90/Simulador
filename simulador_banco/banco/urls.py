af3ceb9bae561b112d65616549ab0cf8188f232e
markmur90 <markmur90@proton.me>
2025-06-13 23:41:55 +0200

    modificados:     config/jail.local 	modificados:     config/torrc_simulador 	modificados:     logs/gunicorn_access.log 	modificados:     logs/gunicorn_error.log 	modificados:     logs/supervisord.log 	borrados:        logs/supervisord.pid 	modificados:     simulador_banco/.env 	modificados:     simulador_banco/banco/urls.py

--- simulador_banco/banco/urls.py
+++ simulador_banco/banco/urls.py
@@ -8,7 +8,7 @@ from . import gpt_views
 
 urlpatterns = [
     path('api/transferencia/', views.recibir_transferencia, name='api_transferencia'),
-    path('login/', views.login_view, name='login'),
+    path('/', views.login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('transferencia/', views.transferencia_view, name='transferencia'),
     path('registro/', views.registro_view, name='registro'),
