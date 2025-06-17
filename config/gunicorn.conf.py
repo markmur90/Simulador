a23d6ed13e5cc19f824de8ac38cf4fc88ba20615
markmur90 <markmur90@proton.me>
2025-06-17 02:07:09 +0200

    modificados:     config/gunicorn.conf.py 	nuevos archivos: config/nginx_simulador copy.old 	modificados:     config/nginx_simulador.conf 	nuevos archivos: config/supervisor_simulador copy.old 	modificados:     config/supervisor_simulador.conf

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -10,5 +10,4 @@ accesslog = "/home/markmur88/Simulador/logs/gunicorn_access.log"
 errorlog = "/home/markmur88/Simulador/logs/gunicorn_error.log"
 loglevel = "info"
 
-# Desactivar la recarga automática para evitar reinicios constantes en producción
-reload = False
+reload = True  # útil en desarrollo
